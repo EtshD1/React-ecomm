bdec50c8f0011629891456f378626392d32ae3f0
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-04-10 17:40:27 +0200

    add HTML

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,8 +20,53 @@ const Login = () => {
       })
   };
 
-
-  return <>Login</>;
+  return context.user ? (
+    <>
+      <div className="hero is-primary ">
+        <div className="hero-body container">
+          <h4 className="title">Login</h4>
+        </div>
+      </div>
+      <br />
+      <br />
+      <form onSubmit={this.login}>
+        <div className="columns is-mobile is-centered">
+          <div className="column is-one-third">
+            <div className="field">
+              <label className="label">Email: </label>
+              <input
+                className="input"
+                type="email"
+                name="username"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="field">
+              <label className="label">Password: </label>
+              <input
+                className="input"
+                type="password"
+                name="password"
+                onChange={this.handleChange}
+              />
+            </div>
+            {this.state.error && (
+              <div className="has-text-danger">{this.state.error}</div>
+            )}
+            <div className="field is-clearfix">
+              <button
+                className="button is-primary is-outlined is-pulled-right"
+              >
+                Submit
+                </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  ) : (
+    <Redirect to="/products" />
+  );
 }
 
 export default Login;
