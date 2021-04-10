5ec334bf5507aab4c189d98daedf29eabe4b6c73
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-04-10 17:39:46 +0200

    login function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import withContext from "../withContext";
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      return this.setState({ error: "Fill all fields!" });
+    }
+    context.login(username, password)
+      .then((loggedIn) => {
+        if (!loggedIn) {
+          this.setState({ error: "Invalid Credentails" });
+        }
+      })
+  };
+
+
   return <>Login</>;
 }
 
