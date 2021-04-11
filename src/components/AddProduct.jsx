5eeb43b6501db3a212632ba674a6c6c8b2e26c21
EtshD1 <mohamed.h.abouelenin@gmail.com>
2021-04-11 20:02:01 +0200

    Add dependencies and states to AddProduct component

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import withContext from "../withContext";
+import { Redirect } from "react-router-dom";
+import axios from 'axios';
+
 
 const AddProduct = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [stock, setStock] = useState('');
+  const [shortDesc, setShortDesc] = useState('');
+  const [description, setscription] = useState('');
+
   return <>
     AddProduct
   </>
